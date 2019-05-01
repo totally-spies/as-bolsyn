54c598740a3791db57a02d0eb37153024864f350
Adilet Absatov <adl.absatov@gmail.com>
2019-05-01 08:01:24 +0600

    Project initialized

---
+++ front/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ClassProvider, NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { ProviderService } from './shared/services/provider.service';
+import { FormsModule } from '@angular/forms';
+import {AuthInterceptor} from './AuthInterceptor';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [
+    ProviderService,
+    <ClassProvider> {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ front/src/app/shared/services/provider.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {MainService} from './main.service';
+import {HttpClient} from '@angular/common/http';
+import {IAuthResponse} from '../models/models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProviderService extends MainService {
+
+  constructor(http: HttpClient) {
+    super(http);
+  }
+
+  auth(login: string, password: string): Promise<IAuthResponse> {
+    return this.post('http://localhost:8000/api/login/', {
+      username: login,
+      password: password
+    });
+  }
+
+  logout(): Promise<any> {
+    return this.post('http://localhost:8000/api/logout/', {});
+  }
+}
