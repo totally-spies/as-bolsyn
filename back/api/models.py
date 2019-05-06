ab02ba152ad0cf0acd83a40300f21994d49dc0a1
Saltanat <snaresova@gmail.com>
2019-05-06 16:07:46 +0600

    Models

--- back/api/models.py
+++ back/api/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Cuisine(models.Model):
+    id = models.IntegerField()
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return '{}:{}'.format('id', 'name')
+
+class Restaurant(models.Model):
+    id = models.IntegerField()
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return '{}:{}'.format('id','name')
+
+class Dish(models.Model):
+    id = models.IntegerField()
+    name = models.CharField()
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{}:{}'.format('name', 'restaurant')
+
+
+class Order(models.Model):
+    id = models.IntegerField()
+    dish_name = models.CharField()
+    count = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{}:{}'.format('id', 'dish_name')
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    text = models.TextField()
+
+    def __str__(self):
+        return '{}:{}'.format('user', 'restaurant')
+
+
