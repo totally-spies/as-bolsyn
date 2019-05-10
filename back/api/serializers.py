47f72254e60eab90fcaf9cd9579b20aa7c2ef17f
Adilet Absatov <adl.absatov@gmail.com>
2019-05-10 17:52:35 +0600

    Getting is_staff managed

--- back/api/serializers.py
+++ back/api/serializers.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('name', 'password', 'login', 'is_superuser')
+        fields = ('username', 'password', 'is_staff')
 
 
 class CuisineSerializer(serializers.Serializer):
