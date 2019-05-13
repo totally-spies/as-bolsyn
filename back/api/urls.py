803871233a09a311f6ad8c6807766786d790a57b
Adilet Absatov <adl.absatov@gmail.com>
2019-05-13 15:41:56 +0600

    Registration implemented. Restaurant model altered

--- back/api/urls.py
+++ back/api/urls.py
@@ -15,5 +15,6 @@ urlpatterns = [
     path('users/', views.UserList.as_view()),
     path('login/', views.Login.as_view()),
     path('logout/', views.Logout.as_view()),
+    path('register/', views.Register.as_view()),
     path('', views.description),
 ]
--- back/api/views/__init__.py
+++ back/api/views/__init__.py
@@ -1,3 +1,3 @@
-from .auth import Login, Logout, UserList, UserInfo
+from .auth import Login, Logout, Register, UserList, UserInfo
 from .views import sections_view, section_view, Restaurants, RestaurantView, \
                     Dishes, DishView, Reviews, Orders, OrderView, Clearer, description
